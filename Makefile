c98540d4cf045e20f11d84a08169a58f1251f745
Maxime Bouillot <maxime.bouillot@withings.com>
2022-09-08 08:13:58 +0200

    Finished merging architecture.

--- Makefile
+++ Makefile
@@ -1,24 +1,10 @@
+CFLAGS ?= -g -Wall -Werror
 
 all: c_alias
 
-# c_alias_mini.sh is c_alias.sh changed into a one-liner
-c_alias_mini.sh: c_alias.sh
-	cat $< | grep -v '#' | grep -v '^$$' | sed -z 's:() {\n:() { :g; s:then\n:then :g; s:\n: ; :g' > $@
-
-# To make c_alias, the argument usage at the end of dc_alias_mini.sh is removed
-# and that one liner is feed back into itself to generate an executable
-# equivalent to the script.
-c_alias: c_alias.c c_alias_mini.sh c_alias.sh
-	BIG_ALIAS=$$(cat c_alias_mini.sh | sed 's:process $$@.*:process:'); \
-	./c_alias.sh $@ "$$BIG_ALIAS"
-
-.PHONY: c_alias_test
-c_alias_test: c_alias c_alias_mini.sh
-	BIG_ALIAS=$$(cat c_alias_mini.sh | sed 's:process $$@.*:process:'); \
-	./c_alias c_alias "\"$$BIG_ALIAS\""
+c_alias: c_alias.c
+	gcc $^ $(CFLAGS) -o $@
 
 clean:
 	rm -rf c_alias
-	rm -rf c_alias_mini.sh
-
 
--- c_alias.c
+++ c_alias.c
@@ -1,19 +1,8 @@
-/*----------------------------------------------\
-|This file is a template to create C-aliases.   |
-|Those are executables that behaves as if they  |
-|were alias to a command. This command should be|
-|given at compile time as the defined constant  |
-|CMD_TO_ALIAS. This defined constant should be a|
-|string literal.                                |
-\----------------------------------------------*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#ifndef CMD_TO_ALIAS
-#error "To compile this file, a string should be passed as the defined value `CMD_TO_ALIAS`. The resulting executable will be an alias to `CMD_TO_ALIAS`."
-#endif
 
 /*
  * Computes the sum of the length of the strings in an array.
@@ -46,7 +35,7 @@ char* str_arr_into_buff(int n, const char** arr) {
  */
 char fold_to_char(int in) {
 	char ret = 0;
-	for (int i=0; i<sizeof(int); i++) {
+	for (size_t i=0; i<sizeof(int); i++) {
 		char mask = (in >> (i * 8)) & 0xFF;
 		ret |= mask;
 	}
@@ -132,7 +121,7 @@ char** process_over_arr_and_free(char** arr, size_t size_arr, char* (func)(const
 /*
  * Display an array of strings.
  */
-void __attribute__((unused)) disp_arr(const char** arr, size_t size_arr) {
+void disp_arr(const char** arr, size_t size_arr) {
 	puts("[");
 	for (size_t i=0; i<size_arr; i++) {
 		printf("%s,\n", arr[i]);
@@ -141,15 +130,12 @@ void __attribute__((unused)) disp_arr(const char** arr, size_t size_arr) {
 }
 
 int main(int argc, const char** argv) {
-	char** processed_arr = process_over_arr(argv+1, argc-1, escape_in_str, '\\', '\\');
-	processed_arr = process_over_arr_and_free(processed_arr, argc-1, escape_in_str, '"', '\\');
-	processed_arr = process_over_arr_and_free(processed_arr, argc-1, surround_string, '"', '"');
-	char* arg_buff = str_arr_into_buff(argc-1, (const char**) processed_arr);
-	free_arr(processed_arr, argc-1);
-	char* exec_buff = malloc(strlen(CMD_TO_ALIAS) + strlen(arg_buff) + 2);
-	strcpy(exec_buff, CMD_TO_ALIAS);
-	strcat(exec_buff, " ");
-	strcat(exec_buff, arg_buff);
+	char** processed_arr = process_over_arr(argv+2, argc-2, escape_in_str, '\\', '\\');
+	processed_arr = process_over_arr_and_free(processed_arr, argc-2, escape_in_str, '"', '\\');
+	char* arg_buff = str_arr_into_buff(argc-2, (const char**) processed_arr);
+	free_arr(processed_arr, argc-2);
+	char* exec_buff = malloc(100000);
+	sprintf(exec_buff, "gcc -o %s c_alias_runtime.c '-DCMD_TO_ALIAS=\"%s\"'", argv[1], arg_buff);
 	int ret = system(exec_buff);
 	free(exec_buff);
 	free(arg_buff);
